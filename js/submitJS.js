59bb43d066d5ef0c66d17f53ca118d0ccaaea516
LAPTOP-Austin <austin2050cheng@gmail.com>
2017-09-21 22:05:54 -0500

    js validation test

---
+++ js/submitJS.js
@@ -0,0 +1,18 @@
+var email = document.getElementById('mail');
+var error = document.querySelector('.error');
+
+email.addEventListener("input", function (event) {
+
+  if (email.validity.valid) {
+
+    error.innerHTML = "";
+    error.className = "error";
+  }
+}, false);
+form.addEventListener("submit", function (event) {
+  if (!email.validity.valid) {
+    error.innerHTML = "invalid email";
+    error.className = "error active";
+    event.preventDefault();
+  }
+}, false);
