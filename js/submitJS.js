b8f1857bffcf2167c704dd7c55ff40dc04b540ee
LAPTOP-Austin <austin2050cheng@gmail.com>
2017-10-12 18:14:50 -0500

    more js fiddling

--- js/submitJS.js
+++ js/submitJS.js
@@ -1,4 +1,5 @@
 
+//party time
 function fff() {
 	document.getElementById("spiral").className = 'party';
 	document.getElementById("surprise").className = 'partyBlock';
@@ -7,6 +8,21 @@ function fff() {
 			document.getElementById("surprise").className = 'hide';
 			}, 3000);
 }
+//make sure form is filled out
+function checkForm() {
+	var check = 0;
+	var fName = document.getElementById("fName").value;
+	var lName = document.getElementById("lName").value;
+	var email = document.getElementById("mail").value;
+	var phone = document.getElementById("phone").value;
+	if( fName == null || lName == null ) {
+			alert("H");
+		}
+	console.log(fName);
+}
 
 var form = document.getElementsByTagName('button')[0];
-form.addEventListener("click", fff, false);
+form.addEventListener("click", checkForm, false);
+
+
+
